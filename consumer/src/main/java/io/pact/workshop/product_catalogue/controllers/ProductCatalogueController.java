7fab2bdc47859a6b9a13a604c8b482623661a9ee
Ronald Holshausen <rholshausen@dius.com.au>
2021-02-25 15:09:36 +1100

    chore: added catalogue and error pages

--- consumer/src/main/java/io/pact/workshop/product_catalogue/controllers/ProductCatalogueController.java
+++ consumer/src/main/java/io/pact/workshop/product_catalogue/controllers/ProductCatalogueController.java
@@ -3,6 +3,7 @@ package io.pact.workshop.product_catalogue.controllers;
 import io.pact.workshop.product_catalogue.clients.ProductServiceClient;
 import io.pact.workshop.product_catalogue.models.ProductCatalogue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,9 @@ public class ProductCatalogueController {
   private ProductServiceClient productServiceClient;
 
   @GetMapping("/catalogue")
-  public ProductCatalogue catalogue() {
-    return new ProductCatalogue("Default Catalogue", productServiceClient.fetchProducts().getProducts());
+  public String catalogue(Model model) {
+    ProductCatalogue catalogue = new ProductCatalogue("Default Catalogue", productServiceClient.fetchProducts().getProducts());
+    model.addAttribute("catalogue", catalogue);
+    return "catalogue";
   }
 }
