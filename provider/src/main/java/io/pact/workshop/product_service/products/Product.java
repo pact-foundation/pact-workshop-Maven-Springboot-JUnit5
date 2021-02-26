d94f73d5db10fce25d8420930847755ff80210cc
Ronald Holshausen <rholshausen@dius.com.au>
2021-02-26 13:02:10 +1100

    chore: step 4

--- provider/src/main/java/io/pact/workshop/product_service/products/Product.java
+++ provider/src/main/java/io/pact/workshop/product_service/products/Product.java
@@ -1,17 +1,18 @@
 package io.pact.workshop.product_service.products;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
   @Id
-  @GeneratedValue(strategy=GenerationType.AUTO)
   private Long id;
   private String name;
   private String type;
---
+++ provider/src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
@@ -0,0 +1,63 @@
+package io.pact.workshop.product_service;
+
+import au.com.dius.pact.provider.junit5.HttpTestTarget;
+import au.com.dius.pact.provider.junit5.PactVerificationContext;
+import au.com.dius.pact.provider.junit5.PactVerificationInvocationContextProvider;
+import au.com.dius.pact.provider.junitsupport.Provider;
+import au.com.dius.pact.provider.junitsupport.State;
+import au.com.dius.pact.provider.junitsupport.StateChangeAction;
+import au.com.dius.pact.provider.junitsupport.loader.PactFolder;
+import io.pact.workshop.product_service.products.Product;
+import io.pact.workshop.product_service.products.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.TestTemplate;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Provider("ProductService")
+@PactFolder("pacts")
+public class PactVerificationTest {
+  @LocalServerPort
+  private int port;
+
+  @Autowired
+  private ProductRepository productRepository;
+
+  @BeforeEach
+  void setup(PactVerificationContext context) {
+    context.setTarget(new HttpTestTarget("localhost", port));
+  }
+
+  @TestTemplate
+  @ExtendWith(PactVerificationInvocationContextProvider.class)
+  void pactVerificationTestTemplate(PactVerificationContext context) {
+    context.verifyInteraction();
+  }
+
+  @State(value = "products exists", action = StateChangeAction.SETUP)
+  void productsExists() {
+    productRepository.deleteAll();
+    productRepository.saveAll(Arrays.asList(
+      new Product(100L, "Test Product 1", "CREDIT_CARD", "v1", "CC_001"),
+      new Product(200L, "Test Product 2", "CREDIT_CARD", "v1", "CC_002"),
+      new Product(300L, "Test Product 3", "PERSONAL_LOAN", "v1", "PL_001"),
+      new Product(400L, "Test Product 4", "SAVINGS", "v1", "SA_001")
+    ));
+  }
+
+  @State(value = "product with ID 10 exists", action = StateChangeAction.SETUP)
+  void productExists(Map<String, Object> params) {
+    long productId = ((Number) params.get("id")).longValue();
+    Optional<Product> product = productRepository.findById(productId);
+    if (!product.isPresent()) {
+      productRepository.save(new Product(productId, "Product", "TYPE", "v1", "001"));
+    }
+  }
+}
