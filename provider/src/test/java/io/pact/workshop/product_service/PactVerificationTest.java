cafe5a207504c31edace7f031503b37c7ad0c9a1
Yousaf Nabi <you@saf.dev>
2024-01-21 00:49:54 +0000

    deps: update to java 17

--- provider/src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
+++ provider/src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
@@ -15,7 +15,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.nio.ByteBuffer;
 import java.util.Arrays;
