d508cf8bd7df68fdbbafc8b53c8316f51f5ab4a0
Yousaf Nabi <you@saf.dev>
2024-01-30 18:47:05 +0000

    chore(deps): pact-jvm 4.6.5 + java17 + springboot 3.1.x

--- consumer/src/main/java/io/pact/workshop/product_catalogue/controllers/ProductCatalogueController.java
+++ consumer/src/main/java/io/pact/workshop/product_catalogue/controllers/ProductCatalogueController.java
@@ -3,11 +3,12 @@ package io.pact.workshop.product_catalogue.controllers;
 import io.pact.workshop.product_catalogue.clients.ProductServiceClient;
 import io.pact.workshop.product_catalogue.models.ProductCatalogue;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
-@RestController
+@Controller
 public class ProductCatalogueController {
   @Autowired
   private ProductServiceClient productServiceClient;
@@ -17,5 +18,11 @@ public class ProductCatalogueController {
     ProductCatalogue catalogue = new ProductCatalogue("Default Catalogue", productServiceClient.fetchProducts().getProducts());
     model.addAttribute("catalogue", catalogue);
     return "catalogue";
+  }
+
+  @GetMapping("/catalogue/{id}")
+  public String catalogue(@PathVariable("id") Long id, Model model) {
+    model.addAttribute("product", productServiceClient.getProductById(id));
+    return "details";
   }
 }
