c7e3516b4c87004952a94750c9d45966fc3fc4ef
Yousaf Nabi <you@saf.dev>
2024-01-30 17:28:18 +0000

    chore(deps): pact-jvm 4.6.5 + java17 + springboot 3.1.x

--- provider/src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
+++ provider/src/test/java/io/pact/workshop/product_service/PactVerificationTest.java
@@ -14,7 +14,7 @@ import org.junit.jupiter.api.TestTemplate;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
 import java.util.Arrays;
 import java.util.Map;
